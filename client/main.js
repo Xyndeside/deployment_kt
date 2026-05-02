be7fb88f8b0e463a9a1e66391732eae9cf90c79d
Daniil Koval <Daniilkoval2006@mail.ru>
2026-05-02 13:21:12 +1000

    upd

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-fetch('/get')
+fetch('http://localhost:3000/get')
     .then(res => res.json())
     .then(data => {
         document.getElementById('output').textContent =
--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
+    host: 'db',
     user: 'admin_kov',
     password: 'Admin123!',
     database: 'sockets_db',
